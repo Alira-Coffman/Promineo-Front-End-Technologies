62c36ad625575474b3105cd68ed0d6b3bccd4484
Alira-Coffman <aliradawn@gmail.com>
2022-02-03 10:43:56 -0600

    More examples

---
+++ Introduction to Javascript/week1/Jan-26/AgeCalculator-3.js
@@ -0,0 +1,6 @@
+var birthYear = 1993;
+var futureYear = 2022;
+
+let ageTurning = futureYear - birthYear;
+var ageCurrent = ageTurning--;
+console.log("I will be either", ageCurrent, "or " + ageTurning + " in", futureYear +".");
--- Introduction to Javascript/week1/test.js
+++ Introduction to Javascript/week1/test.js
@@ -1,5 +1,19 @@
-var x = 7;
-var y = 8;
+//var, let, const
 
-console.log(x+y);
+var x = 8;
+let y = 7;
+const z = 'Hello';
+console.log(x , y, z);
 
+var newVariable; //DECLARATION
+let secondVariable = "hello";
+console.log(secondVariable)
+
+console.log(x > y || x > 0);
+
+//Bool Operators
+// && - and both statments are true
+// || - or at least one statment is true
+
+var myVariable;
+myVariable = "name";
